1d0a7d4a4040a9641a67f86790519502ff7b5826
xp-1000 <xp-1000@hotmail.fr>
2016-10-15 13:09:37 +0200

    Replace kmix by plasma-pa

--- kde.sh
+++ kde.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 sudo pacman -S --noconfirm plasma kdebase kde-l10n-fr 
-sudo pacman -S --noconfirm yakuake phonon-qt4-vlc phonon-qt5-vlc kscreen kmix hunspell-fr spectacle kdegraphics-okular breeze-gtk ark
+sudo pacman -S --noconfirm yakuake phonon-qt4-vlc phonon-qt5-vlc kscreen hunspell-fr spectacle kdegraphics-okular breeze-gtk ark
 sudo pacman -Rsn --noconfirm plasma-mediacenter kdebase-konqueror kdebase-konq-plugins kate 
-sudo pacman -R --noconfirm plasma-pa
 sudo pacman -S --noconfirm ttf-freefont ttf-dejavu ttf-liberation networkmanager atom
 sudo systemctl enable sddm
 sudo systemctl enable NetworkManager
