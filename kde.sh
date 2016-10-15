ca24d1b6c6749ed7040db772600222d852904ab8
xp-1000 <xp-1000>
2016-10-15 15:57:14 +0200

    Add printer support

--- kde.sh
+++ kde.sh
@@ -2,9 +2,10 @@
 sudo pacman -S --noconfirm plasma kdebase kde-l10n-fr 
 sudo pacman -S --noconfirm yakuake phonon-qt4-vlc phonon-qt5-vlc kscreen hunspell-fr spectacle kdegraphics-okular breeze-gtk ark
 sudo pacman -Rsn --noconfirm plasma-mediacenter kdebase-konqueror kdebase-konq-plugins kate 
-sudo pacman -S --noconfirm ttf-freefont ttf-dejavu ttf-liberation networkmanager atom
-sudo systemctl enable sddm
-sudo systemctl enable NetworkManager
+sudo pacman -S --noconfirm ttf-freefont ttf-dejavu ttf-liberation networkmanager atom cups print-manager
+sudo systemctl enable sddm.service
+sudo systemctl enable NetworkManager.service
+sudo systemctl enable cups-browsed.service
 sudo sed -i 's/^Current=$/Current=breeze/' /etc/sddm.conf
 sudo sed -i 's/^CursorTheme=$/CursorTheme=breeze_cursors/' /etc/sddm.conf
 sudo sed -i 's/^Numlock=none$/Numlock=on/' /etc/sddm.conf
@@ -12,10 +13,10 @@ mkdir ${HOME}/.compose-cache/
 ln -sfv /run/user/$UID/ /home/$USER/.compose-cache
 echo -e '\n[Windows]\nBorderlessMaximized=true' >> ${HOME}/.config/kwinrc
 yaourt -S --noconfirm firefox-kde-opensuse
-pacman -S firefox-i18n-fr
+pacman -S --noconfirm firefox-i18n-fr
 mv -f "`dirname $0`/files/kde/autostart/*.desktop"  $HOME/.config/autostart/
-systemctl start sddm
+sudo systemctl start sddm.service
 # themes arc 
 # shortcuts + reglages kde generaux ?
-# Boulot seulement : terraform  + restaurer le backup firefox et cie
+# terraform  + restaurer le backup firefox et cie
 
