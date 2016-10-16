61cac796ea779311cfc5e13aeedf39cc684fef15
xp-1000 <xp-1000>
2016-10-16 13:33:12 +0200

    Import kde general config

--- kde.sh
+++ kde.sh
@@ -9,14 +9,39 @@ sudo systemctl enable cups-browsed.service
 sudo sed -i 's/^Current=$/Current=breeze/' /etc/sddm.conf
 sudo sed -i 's/^CursorTheme=$/CursorTheme=breeze_cursors/' /etc/sddm.conf
 sudo sed -i 's/^Numlock=none$/Numlock=on/' /etc/sddm.conf
-mkdir ${HOME}/.compose-cache/
-ln -sfv /run/user/$UID/ /home/$USER/.compose-cache
-echo -e '\n[Windows]\nBorderlessMaximized=true' >> ${HOME}/.config/kwinrc
 yaourt -S --noconfirm firefox-kde-opensuse
 pacman -S --noconfirm firefox-i18n-fr
 mv -f "`dirname $0`/files/kde/autostart/*.desktop"  $HOME/.config/autostart/
+mv -f "`dirname $0`/files/kde/config/*"  $HOME/.config/
+mv -f "`dirname $0`/files/kde/kde4/share/config/*"  $HOME/.kde4/share/config/
+mkdir ${HOME}/.compose-cache/
+ln -sfv /run/user/$UID/ /home/$USER/.compose-cache
+echo -e '\n[Windows]\nBorderlessMaximized=true' >> ${HOME}/.config/kwinrc
+echo "gtk-primary-button-warps-slider=false" >> ${HOME}/.config/gtk-3.0/settings.ini
+cat <<EOF >  ${HOME}/.config/kscreenlockerrc
+[Daemon]
+LockGrace=30
+Timeout=15
+EOF
+cat <<EOF >  ${HOME}/.config/plasma-localerc
+[Translations]
+LANGUAGE=fr
+EOF
+cat <<EOF >  ${HOME}/.config/systemsettingsrc
+[Basic Settings]
+Indexing-Enabled=false
+EOF
+sed -i '1i[Basic Settings]\nIndexing-Enabled=false' ${HOME}/.config/baloofilerc
+sed -i '/\[General\]/a\BrowserApplication[$e]=!firefox' ${HOME}/.kde4/share/config/kdeglobals
+sed -i '/\[General\]/a\BrowserApplication[$e]=!firefox' ${HOME}/.config/kdeglobals
+sed -i '/\[KDE\]/a\DoubleClickInterval=800\nSingleClick=true\nStartDragDist=4\nStartDragTime=500\nWheelScrollLines=3' ${HOME}/.config/kdeglobals
+sed -i 's/^loginMode.*$/loginMode=default/' ${HOME}/.config/ksmserverrc
+sed -i 's/Switch to Next Desktop=none/Switch to Next Desktop=Ctrl+Alt+Right/' ${HOME}/.config/kglobalshortcutsrc
+sed -i 's/Switch to Previous Desktop=none/Switch to Previous Desktop=Ctrl+Left+Right/' ${HOME}/.config/kglobalshortcutsrc
+sed -i 's/Walk Through Windows (Reverse)=Alt+Shift+Backtab/Walk Through Windows (Reverse)=Alt+Shift+Tab/' ${HOME}/.config/kglobalshortcutsrc
+
+
 sudo systemctl start sddm.service
 # themes arc 
-# shortcuts + reglages kde generaux ?
-# terraform  + restaurer le backup firefox et cie
+# shortcuts 
 
