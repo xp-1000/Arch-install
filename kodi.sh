ac63981b5aa97b01be9ead64ef47ffcd670b967c
xp-1000 <xp-1000@hotmail.fr>
2016-12-10 20:44:59 +0100

    Add torrent support

--- kodi.sh
+++ kodi.sh
@@ -25,7 +25,8 @@ if [ -z "\$DISPLAY" ] && [ \$(tty) == /dev/tty1 ]; then
     startx
 fi
 EOF
-sudo pacman --noconfirm -S kodi kodi-pvr-addons
+sudo pacman --noconfirm -S kodi
+yaourt --noconfirm -S kodi-addon-pvr-iptvsimple-git
 echo "kodi &" >> ${HOME}/.config/openbox/autostart 
 echo "sakura &" >> ${HOME}/.config/openbox/autostart 
 yaourt --noconfirm -S google-chrome
@@ -38,8 +39,6 @@ sudo pacman --noconfirm -S steam steam-native-runtime pulseaudio wmctrl libxcb
 sudo pacman --noconfirm -S lib32-openal lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-gconf lib32-dbus-glib lib32-libnm-glib lib32-libudev0-shim lib32-alsa-plugins lib32-libpulse lib32-libxcb lib32-curl
 wget http://tv.manfroi.fr/res/kodi-backup.zip -O /tmp/kodi-backup.zip
 rm -fr ${HOME}/.kodi
-mkdir -p ${HOME}/.kodi
-unzip /tmp/kodi-backup.zip -d ${HOME}/.kodi/
-folder=$(ls ${HOME}/.kodi/ | sort | head -n 1)
-mv ${HOME}/.kodi/${folder}/* ${HOME}/.kodi/
-rm -fr ${HOME}/.kodi/${folder}
+unzip /tmp/kodi-backup.zip -d ${HOME}
+mkdir -p ${HOME}/{Vidéos/Séries,Vidéos/Films,Musique,Images}
+cd && wget "$(curl -s https://api.github.com/repos/scakemyer/plugin.video.quasar/releases | grep browser_download_url | grep linux_x64 | head -n 1 | cut -d '"' -f 4)"
