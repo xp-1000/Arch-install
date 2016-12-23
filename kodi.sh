481d439998287544f87e190e45305fb4ac41c593
XP-1000 <xp-1000@hotmail.fr>
2016-12-23 22:48:17 +0100

    fix archive format

--- kodi.sh
+++ kodi.sh
@@ -42,6 +42,6 @@ sudo pacman --noconfirm -S steam steam-native-runtime pulseaudio wmctrl libxcb
 sudo pacman --noconfirm -S lib32-openal lib32-nss lib32-gtk2 lib32-gtk3 lib32-libcanberra lib32-gconf lib32-dbus-glib lib32-libnm-glib lib32-libudev0-shim lib32-alsa-plugins lib32-libpulse lib32-libxcb lib32-curl
 wget http://tv.manfroi.fr/res/kodi-kiwisbox.tar.gz -O /tmp/kodi-backup.tar.gz
 rm -fr ${HOME}/.kodi
-tar xvzf /tmp/kodi-backup.zip -C ${HOME}
+tar xvzf /tmp/kodi-backup.tar.gz -C ${HOME}
 mkdir -p ${HOME}/{Videos/Series,Videos/Films,Musique,Images,Telechargements/Films,Telechargements/Series}
 #cd && wget "$(curl -s https://api.github.com/repos/scakemyer/plugin.video.quasar/releases | grep browser_download_url | grep linux_x64 | head -n 1 | cut -d '"' -f 4)"
