3765d7c4398576179f65203ad6b6b6a2d07fbd82
xp-1000 <xp-1000@hotmail.fr>
2017-01-03 17:11:38 +0100

    Add volume notification for openbox

--- openbox.sh
+++ openbox.sh
@@ -4,6 +4,8 @@ set -e
 set -x
 sudo usermod -a -G audio,video,users,storage,disk,power,wheel $USER
 sudo pacman --noconfirm -S openbox
+yaourt -S libaosd --noconfirm
+sudo cp -f `dirname $0`/files/openbox/osd-mixer.sh /usr/local/bin/
 mkdir -p ${HOME}/.config/
 cp -r /etc/xdg/openbox ${HOME}/.config/
 echo "exec openbox-session" > ${HOME}/.xinitrc
@@ -11,17 +13,17 @@ cat <<EOF >> /tmp/add.txt
   <!-- Audio controls --> 
   <keybind key="XF86AudioRaiseVolume">
     <action name="Execute">
-        <command>amixer set Master 5%+ unmute</command>
+        <command>osd-mixer.sh volup</command>
     </action>
   </keybind>
   <keybind key="XF86AudioLowerVolume">
     <action name="Execute">
-        <command>amixer set Master 5%- unmute</command>
+        <command>osd-mixer.sh voldown</command>
     </action>
   </keybind>
   <keybind key="XF86AudioMute">
     <action name="Execute">
-        <command>amixer set Master toggle</command>
+        <command>osd-mixer.sh mute</command>
     </action>
    </keybind>
 EOF
