393b79e15f972e8446ebe99d2d1ba9e13c8b3f6c
xp-1000 <xp-1000@hotmail.fr>
2019-03-16 19:35:24 +0100

    update kde config

--- 01-intel.sh
+++ 01-intel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set -uxo pipefail
+set -euo pipefail
 
 pacman --noconfirm -S mesa xf86-video-intel vulkan-intel libva-intel-driver libvdpau-va-gl vdpauinfo
--- 02-kde.sh
+++ 02-kde.sh
@@ -9,8 +9,9 @@ if [[ $EUID -eq 0 ]]; then
 fi
 yay -S --noconfirm plasma-meta plasma-wayland-session 
 yay -S --noconfirm yakuake phonon-qt5-vlc kscreen hunspell-fr spectacle okular breeze-gtk ark
-#yay -Rsn --noconfirm kdebase-konqueror kdebase-konq-plugins kate 
+yay -Rsn --noconfirm konqueror kate 
 yay -S --noconfirm ttf-freefont ttf-dejavu ttf-liberation networkmanager atom cups print-manager
+yay -S --noconfirm papirus-icon-theme arc-gtk-theme arc-kde kvantum-theme-arc kvantum-qt5 kde-gtk-config
 sudo systemctl enable sddm.service
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable cups-browsed.service
@@ -36,31 +37,26 @@ ln -sfv /run/user/$UID/ ${HOME}/.compose-cache
 cp -f $(dirname $0)/files/kde/autostart/*.desktop  $HOME/.config/autostart/
 cp -f $(dirname $0)/files/kde/config/*  $HOME/.config/
 cp -f $(dirname $0)/files/kde/kde4/share/config/*  $HOME/.kde4/share/config/
-echo -e '\n[Windows]\nBorderlessMaximized=true' >> ${HOME}/.config/kwinrc
 echo "gtk-primary-button-warps-slider=false" >> ${HOME}/.config/gtk-3.0/settings.ini
-cat <<EOF >  ${HOME}/.config/kscreenlockerrc
-[Daemon]
-LockGrace=30
-Timeout=15
-EOF
-cat <<EOF >  ${HOME}/.config/plasma-localerc
-[Translations]
-LANGUAGE=fr
-EOF
-cat <<EOF >  ${HOME}/.config/systemsettingsrc
-[Basic Settings]
-Indexing-Enabled=false
-EOF
-sed -i '1i[Basic Settings]\nIndexing-Enabled=false' ${HOME}/.config/baloofilerc
-sed -i '/\[General\]/a\BrowserApplication[$e]=!firefox' ${HOME}/.kde4/share/config/kdeglobals
-sed -i '/\[General\]/a\BrowserApplication[$e]=!firefox' ${HOME}/.config/kdeglobals
-sed -i '/\[KDE\]/a\DoubleClickInterval=800\nSingleClick=true\nStartDragDist=4\nStartDragTime=500\nWheelScrollLines=3' ${HOME}/.config/kdeglobals
+sed -i 's/^gtk-icon-theme-name=/gtk-icon-theme-name=Papirus-Dark/g' ${HOME}/.config/gtk-3.0/settings.ini
+sed -i 's/^gtk-theme-name=.*/gtk-theme-name=Arc-Dark/g' ${HOME}/.config/gtk-3.0/settings.ini
+sed -i 's/^gtk-icon-theme-name=.*/gtk-icon-theme-name="Papirus-Dark"/g' ${HOME}/.gtkrc-2.0
+sed -i 's/^gtk-theme-name=.*/gtk-theme-name="Arc-Dark"/g' ${HOME}/.gtkrc-2.0
+sed -i 's/^include .*/include "\/usr\/share\/themes\/Arc-Dark\/gtk-2.0\/gtkrc"/g' ${HOME}/.gtkrc-2.0
+sed -i 's/^ksplashrc_ksplash_theme=.*/ksplashrc_ksplash_theme=com.github.varlesh.arc-dark/g' ${HOME}/.config/startupconfig
+echo -e '\n[Greeter]\nTheme=com.github.varlesh.arc-dark' >> .config/kscreenlockerrc
+sed -i 's/^Theme=.*/Theme=Papirus-Dark/g' ${HOME}/.kde4/share/config/kdeglobals ${HOME}/.config/kdeglobals
+sed -i 's/^ColorScheme=.*/ColorScheme=Arc-Dark/g' ${HOME}/.kde4/share/config/kdeglobals ${HOME}/.config/kdeglobals
+sed -i 's/^widgetStyle=.*/widgetStyle=kvantum-dark/g' ${HOME}/.kde4/share/config/kdeglobals ${HOME}/.config/kdeglobals
+sed -i '/^\[KDE\]/a LookAndFeelPackage=com.github.varlesh.arc-dark' ${HOME}/.kde4/share/config/kdeglobals ${HOME}/.config/kdeglobals
+sed -i '/\[General\]/a\BrowserApplication[$e]=!firefox' ${HOME}/.kde4/share/config/kdeglobals ${HOME}/.config/kdeglobals
+sed -i '/\[KDE\]/a\DoubleClickInterval=800\nSingleClick=true\nStartDragDist=4\nStartDragTime=500\nWheelScrollLines=3' ${HOME}/.kde4/share/config/kdeglobals  ${HOME}/.config/kdeglobals
+sed -i 's/^show-on-mouse-pos=none,none,\(.*\)$/show-on-mouse-pos=Ctrl+Alt+C,none,\1/' ${HOME}/.config/kglobalshortcutsrc
 sed -i 's/Switch to Next Desktop=none/Switch to Next Desktop=Ctrl+Alt+Right/' ${HOME}/.config/kglobalshortcutsrc
 sed -i 's/Switch to Previous Desktop=none/Switch to Previous Desktop=Ctrl+Alt+Left/' ${HOME}/.config/kglobalshortcutsrc
 sed -i 's/Walk Through Windows (Reverse)=Alt+Shift+Backtab/Walk Through Windows (Reverse)=Alt+Shift+Tab/' ${HOME}/.config/kglobalshortcutsrc
+sed -i '1i[Basic Settings]\nIndexing-Enabled=false' ${HOME}/.config/baloofilerc
 sudo systemctl restart sddm.service
-# themes arc kde
-# shortcuts kde
 ## kdewallet for ssh key support
 # export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 # export SSH_ASKPASS="/usr/bin/ksshaskpass"
